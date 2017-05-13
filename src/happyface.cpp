7fe8b14fa6d77fc40fd38ee67531588938037b9b
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-05-13 12:44:27 +0200

    :D

--- src/happyface.cpp
+++ src/happyface.cpp
@@ -1,14 +1,12 @@
 #include <fstream>
 #include <iostream>
 #include <map>
-#include <string>
-#include <vector>
 #include <sstream>
+#include <string>
 
-using namespace std;
+using std::string;
 
 string get_argument(int argc, char* argv[], string argument_to_find) {
-	vector<string> arguments;
 	string argument;
 	for (int i = 1; i < argc; ++i) {
 		if (string(argv[i]) == argument_to_find) {
@@ -17,8 +15,6 @@ string get_argument(int argc, char* argv[], string argument_to_find) {
 			} else {
 				argument = "";
 			}
-		} else {
-			arguments.push_back(argv[i]);
 		}
 	}
 	return argument;
@@ -27,87 +23,87 @@ string get_argument(int argc, char* argv[], string argument_to_find) {
 int main(int argc, char* argv[]) {
 	string input_file = get_argument(argc, argv, "-i");
 	if (input_file == "") {
-		cerr << "Please specify input file with -i flag." << endl;
+		std::cerr << "Please specify input file with -i flag." << std::endl;
 	}
 
-	string output_file = get_argument(argc, argv, "-o");
+	const string TO_BRAINFUCK = "to_b";
+	const string TO_SMILEY = "to_s";
 
+	string output_file = get_argument(argc, argv, "-o");
 	string transpiler_mode = get_argument(argc, argv, "-c");
 	if (transpiler_mode == "") {
 		// If transpile mode is empty, try to guess what the user wants
-		if (input_file.substr(input_file.find_last_of(".") + 1) == "s") {
-			// Input file ends with s, defaulting Smiley to Brainfuck mode
-			transpiler_mode = "to_b";
-		} else if (input_file.substr(input_file.find_last_of(".") + 1) == "b") {
-			// Input file ends with b, defaulting Brainfuck to Smiley mode
-			transpiler_mode = "to_s";
+		if (output_file.substr(output_file.find_last_of(".") + 1) == "b") {
+			// Output file ends with b, defaulting Smiley to Brainfuck mode
+			transpiler_mode = TO_BRAINFUCK;
+		} else if (output_file.substr(output_file.find_last_of(".") + 1)
+				== "s") {
+			// Output file ends with s, defaulting Brainfuck to Smiley mode
+			transpiler_mode = TO_SMILEY;
 		} else {
-			cerr << "Please specify transcompile mode with -c flag." << endl;
+			std::cerr << "Please specify transcompile mode with -c flag." << std::endl;
 		}
-	} else if (transpiler_mode != "to_b" && transpiler_mode != "to_s") {
-		cerr << "Please specify a valid transcompile mode." << endl;
+	} else if (transpiler_mode != TO_BRAINFUCK
+			&& transpiler_mode != TO_SMILEY) {
+		std::cerr << "Please specify a valid transcompile mode." << std::endl;
 	}
 
-	map<string, string> to_b;
+	std::map<string, string> to_b;
 	to_b[":>"] = ">";
 	to_b[":<"] = "<";
-	to_b[":-)"] = "+";
-	to_b[":-("] = "-";
+	to_b[":)"] = "+";
+	to_b[":("] = "-";
 	to_b[":O"] = ".";
 	to_b[";)"] = ",";
 	to_b[":["] = "[";
 	to_b[":]"] = "]";
 
-	map<string, string> to_s;
+	std::map<string, string> to_s;
 	to_s[">"] = ":>";
 	to_s["<"] = ":<";
-	to_s["+"] = ":-)";
-	to_s["-"] = ":-(";
+	to_s["+"] = ":)";
+	to_s["-"] = ":(";
 	to_s["."] = ":O";
 	to_s[","] = ";)";
 	to_s["["] = ":[";
 	to_s["]"] = ":]";
 
-	map<string, string> m = (transpiler_mode == "to_b" ? to_b : to_s);
+	std::map<string, string> m = (transpiler_mode == TO_BRAINFUCK ? to_b : to_s);
 
-	ifstream in;
+	std::ifstream in;
 	in.open(input_file);
 
 	if (in.is_open()) {
 		string result;
 		string line;
-		if (transpiler_mode == "to_b") {
-			while (getline(in, line)) {
+		if (transpiler_mode == TO_BRAINFUCK) {
+			while (std::getline(in, line)) {
 				// Transcompile Smiley to Brainfuck
-				stringstream ss(line);
+				std::stringstream ss(line);
 				string c;
 				while (getline(ss, c, ' ')) {
-					if (m.find(c) != m.end()) {
-						result += m[c];
-					}
+					result += m[c];
 				}
 			}
-		} else if (transpiler_mode == "to_s") {
+		} else if (transpiler_mode == TO_SMILEY) {
 			while (getline(in, line)) {
 				// Transcompile Brainfuck to Smiley
 				for (size_t i = 0; i < line.size(); ++i) {
 					string c(1, line[i]);
-					if (m.find(c) != m.end()) {
-						result += m[c] + " ";
-					}
+					result += m[c] + " ";
 				}
 			}
 		}
-		in.close();
 
-		ofstream out;
+		std::ofstream out;
 		if (output_file == "") {
-			string extension = (transpiler_mode == "to_b" ? ".b" : ".s");
+			string extension = (transpiler_mode == TO_BRAINFUCK ? ".b" : ".s");
 			output_file = "output" + extension;
 		}
 		out.open(output_file);
-		out << result << endl;
+		out << result << std::endl;
 		out.close();
 	}
+
 	return 0;
 }
